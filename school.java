9808f6cd6f3588b96b02cdc52d46bedde5448f1a
Tim_o_c <timothy.chhor24@ycdsbk12.ca>
2024-05-28 12:18:23 -0400

    imports for the main file

--- school.java
+++ school.java
@@ -1,9 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.*;
+import java.util.Timer.*;
+
 public class school{
     // Properties
-    int int
+    JFrame theframe;
+    animationpanel schoolPanel = new animationpanel();
+    Timer thetimer;
 
     // Methods
 
     // Constructor
-
+    public static void main(String[] args){
+        new school();
+    }
 }
