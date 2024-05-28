9fb15be26076dd9582f20ade18cb3c77a1479c45
choi-c <carmenchoi333@gmail.com>
2024-05-28 09:25:07 -0400

    added animation panel

---
+++ animationpanel.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.imageio.*;
+import java.awt.image.*;
+
+public class animationpanel {
+    // Properties
+    BufferedImage imgFloor = null, imgSupport = null, imgTable = null, imgChair = null;
+
+    // Methods
+
+    // Constructor
+    try{
+        imgFloor = ImageIO.read(new File(""));
+    } catch(IOException e){
+        System.out.println("Unable to load image");
+    }
+}
