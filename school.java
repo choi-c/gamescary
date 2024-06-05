e292114e30b2761924910a7070e0b1b8b610fdc4
Tim_o_c <timothy.chhor24@ycdsbk12.ca>
2024-06-04 21:11:46 -0400

    loaded images for game

--- school.java
+++ school.java
@@ -1,13 +1,53 @@
+import java.awt.*;
+import javax.swing.*;
 import java.io.*;
 import javax.imageio.*;
 import java.awt.image.*;
 
-public class school{
+public class school extends JPanel{
     // Properties
+    BufferedImage hider1,hider2,chair,floor,gym,locker,pillar,principal,seeker1,seeker2,table,wall = null;
 
     // Methods
+    public BufferedImage loadImage(String strFileName){
+        InputStream imageclass = null;
+        imageclass = this.getClass().getResourceAsStream(strFileName);
+        if (imageclass == null){
+            return null;
+        }else{
+            try{
+                return ImageIO.read(imageclass);
+            }catch(IOException e){
+                return null;
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        
+    }
 
     // Constructor
-    
+    public school(){
+        super();
+        try{
+            hider1 = ImageIO.read(new File("hider1.png"));
+            hider2 = ImageIO.read(new File("hider2.png"));
+            chair = ImageIO.read(new File("chair.png"));
+            floor = ImageIO.read(new File("floor.png"));
+            gym = ImageIO.read(new File("gym.png"));
+            locker = ImageIO.read(new File("locker.png"));
+            pillar = ImageIO.read(new File("pillar.png"));
+            principal = ImageIO.read(new File("principal.png"));
+            seeker1 = ImageIO.read(new File("seeker1.png"));
+            seeker2 = ImageIO.read(new File("seeker2.png"));
+            table = ImageIO.read(new File("table.png"));
+            wall = = ImageIO.read(new File("wall.png"));
 
+        }catch(IOException e){
+            System.out.println("Unable to load image");
+        }
+
+    }
 }
