884f58de88ed21df631e6de84acf3ef27cdd8626
choi-c <carmenchoi333@gmail.com>
2024-06-05 09:42:46 -0400

    printing map

--- school.java
+++ school.java
@@ -6,8 +6,8 @@ import java.awt.image.*;
 
 public class school extends JPanel{
     // Properties
-    BufferedImage hider1,hider2,chair,floor,gym,locker,pillar,principal,seeker1,seeker2,table,wall = null;
-    String[][] strMap = new String[20][20];
+    BufferedImage hider1,hider2,chair,floor,gym,locker,pillar,principal,seeker1,seeker2,desk,wall = null;
+    String[][] strMap = new String[30][40];
     int playerX = 2;
     int playerY = 29;
 
@@ -25,25 +25,69 @@ public class school extends JPanel{
             }
         }
     }
+    public void loadMap(int intPX, int intPY){
+        
+    }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(seeker1, 75, 133, 200, 267, null);
-    }
-    public BufferedImage letterImage(String letter){
-        if(letter.equals("c")){
-            return chair;
-        }else if(letter.equals("l")){
-            return locker;
-        }else if(letter.equals("s")){
-            return pillar;
-        }else if(letter.equals("p")){
-            return principal;
-        }else if(letter.equals("t")){
-            return table;
-        }else if(letter.equals("l")){
-            return wall;
+
+        String[][] strMapShown = new String[9][9];
+        BufferedReader fileMap = null;
+
+        try{
+            fileMap = new BufferedReader(new FileReader("school.csv"));
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
         }
+
+        int intRow;
+		int intCol;
+        String strLine = null;
+        String strSplit[];
+		for(intRow = 0; intRow < 20; intRow++){
+            try{
+                strLine = fileMap.readLine();
+            }catch(IOException e){
+                System.out.println("File not found");
+            }
+			for(intCol = 0; intCol < 20; intCol++){
+				strSplit = strLine.split(",");
+				strMap[intRow][intCol] = strSplit[intCol];
+			}
+		}
+        try{
+		    fileMap.close();
+        }catch(IOException e){
+            System.out.println("File not found");
+        }
+
+        //g.drawImage(hider1, intX, intY, 80, 80, null);
+
+        int intX = 0;
+		int intY = 0;
+		for(intRow = 0; intRow < 20; intRow++){
+			for(intCol = 0; intCol < 20; intCol++){
+				if(strMap[intRow][intCol].equals("l")){
+					g.drawImage(locker, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("c")){
+					g.drawImage(chair, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("d")){
+					g.drawImage(desk, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("p")){
+					g.drawImage(principal, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("s")){
+					g.drawImage(pillar, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("f")){
+					g.drawImage(floor, intX, intY, 80, 80, null);
+				}else if(strMap[intRow][intCol].equals("w")){
+					g.drawImage(wall, intX, intY, 80, 80, null);
+				}
+				intX = intX + 80;
+			}
+			intX = 0;
+			intY = intY + 80;
+		}
     }
 
     // Constructor
@@ -60,7 +104,7 @@ public class school extends JPanel{
             principal = ImageIO.read(new File("principal.png"));
             seeker1 = ImageIO.read(new File("seeker1.png"));
             seeker2 = ImageIO.read(new File("seeker2.png"));
-            table = ImageIO.read(new File("table.png"));
+            desk = ImageIO.read(new File("table.png"));
             wall = ImageIO.read(new File("wall.png"));
 
         }catch(IOException e){
