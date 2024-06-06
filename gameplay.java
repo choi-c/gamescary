44482edb3d71a4e1161e1b0e1f00aeccec135d89
choi-c <carmenchoi333@gmail.com>
2024-06-06 10:25:25 -0400

    added gameplay logic (incomplete)

--- gameplay.java
+++ gameplay.java
@@ -1,9 +1,27 @@
-public class gameplay {
+public class gameplay extends scary{
     // Properties
-    //String strMap[][] = new String[][];
-
+    boolean blnVisibility = false;
+    boolean blnAlive = true;
+    int intChrChoice = 0;
+    int intH1X,intH1Y;
+    int intS1X, intS1Y;
+    int intH2X,intH2Y;
 
     // Methods
+    public void characterBuffs(int intChrChoice,boolean blnVisibility){
+        // if seeker chooses eyeball skin
+        if(intChrChoice == 1){
+            blnVisibility = true;
+        // if seeker chooses monester skin
+        }else if(intChrChoice == 2){
+
+        }
+    }
+    public boolean tag(int intChrChoice,int intH1X,int intH2Y,int intS1X,int intS1,int intH2X,int intH2Y){
+        if(intChrChoice == 1){
+
+        }
+    }
 
     // Constructor
     public gameplay(){
